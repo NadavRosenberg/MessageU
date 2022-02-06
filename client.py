3698a16435de8ed3646e912818f5e6518e9b773e
nadav.rosenberg <rosenbergnadav@gmail.com>
2022-02-07 01:35:31 +0200

    Add project files.

---
+++ client.py
@@ -0,0 +1,11 @@
+class Client(object):
+    def __init__(self, id, user_name, public_key, last_seen):
+        self.id = id
+        self.user_name = user_name
+        self.public_key = public_key
+        self.last_seen = last_seen
+    pass
+
+
+
+
---
+++ message.py
@@ -0,0 +1,12 @@
+class Message(object):
+    def __init__(self, id, to_client, from_client, type, content):
+        self.id = id
+        self.to_client = to_client
+        self.from_client = from_client
+        self.type = type
+        self.content = content
+    pass
+
+
+
+
